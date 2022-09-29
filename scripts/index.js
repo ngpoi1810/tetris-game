fed64a5d1c1f8b8e3ab1bd88965f8cbb3635e77c
nguyen hai nguyen <nguyenhainguyen@bincg.com>
2022-09-29 17:03:47 +0700

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+// CONSTANT
+const COLS = 10;
+const ROWS = 20;
+const BLOCK_SIZE = 30;
+const COLOR_MAPPING = [
+    'red',
+    'orange',
+    'green',
+    'purple',
+    'blue',
+    'cyan',
+    'yellow',
+    'white',
+]
+
+const WHITE_COLOR_ID = 7;
+
+const canvas = document.getElementById('board');
+const ctx = con
